0bec9b41e4d21eb45777c3b2fc6db68f7852327f
Evgeni Obuchowski <evgeni@obukhovski.com>
2023-12-04 23:44:47 +0100

    update user input

--- src/handlers/tg-bot.mjs
+++ src/handlers/tg-bot.mjs
@@ -1,69 +1,46 @@
-import { Telegraf } from 'telegraf';
-import https from 'https';
-import sharp from 'sharp';
+import { Telegraf, Markup, session } from 'telegraf';
+import categories from './categories.js';
 
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN, { telegram: { webhookReply: false } });
+bot.use(session());
 
-bot.start(ctx => ctx.reply('Welcome'));
-bot.help(ctx => ctx.reply('Send me a sticker'));
-bot.hears('hi', ctx => ctx.reply('Hey there'));
-
-bot.on('message', async (ctx) => {
-  if (ctx.message.sticker) {
-    await ctx.reply('👍');
-  } else if (ctx.message.photo || (ctx.message.document && ctx.message.document.mime_type.indexOf('image/') === 0)) {
-    await imageTransform(ctx);
-  } else {
-    await ctx.reply('Unsupported request.')
+bot.hears(categories.flat(), async (ctx) => {
+  if (!ctx.session?.amount) {
+    return ctx.reply('Please start by entering your spending.');
   }
+
+  const category = ctx.message.text;
+  const { amount, comment } = ctx.session;
+
+  // Submit data to Google Form
+  await submitToGoogleForm(amount, comment, category);
+  ctx.session.amount = ctx.session.comment = undefined;
+  return ctx.reply(`Submitted: ${amount}zł for ${category}${comment ? ` - ${comment}` : ''}`);
 });
 
-const getFileBuffer = (fileLink) => {
-  return new Promise((resolve, reject) => {
-    https.get(fileLink, (res) => {
-      const chunks = [];
-      res.on("data", (chunk) => {
-        chunks.push(chunk);
-      });
-      res.on("end", () => {
-        resolve(Buffer.concat(chunks));
-      });
-      res.on("error", (err) => {
-        reject(err);
-      });
-    });
-  });
-};
+bot.on('text', (ctx) => {
+  const text = ctx.message.text;
+  const [amount, ...commentParts] = text.split(' ');
+  const comment = commentParts.join(' ');
 
-const imageTransform = async (ctx) => {
-  try {
-    let fileId;
-    let fileName = 'image.png';
-
-    if (ctx.message.photo) {
-      fileId = ctx.message.photo[ctx.message.photo.length - 1].file_id;
-    } else {
-      fileId = ctx.message.document.file_id;
-      fileName = `${ctx.message.document.file_name.split('.')[0]}.png`;
-    }
-
-    const fileLink = await ctx.telegram.getFileLink(fileId);
-    const buffer = await getFileBuffer(fileLink);
-    const pngBuffer = await sharp(buffer)
-    .png()
-    .toBuffer();
-
-    await ctx.replyWithDocument(
-      {
-        source: Buffer.from(pngBuffer),
-        filename: fileName,
-        mimeType: 'image/png'
-      }
-    );
-  } catch (e) {
-    console.error(e);
-    await ctx.reply('Send me an image to convert to PNG or a sticker.');
+  if (isNaN(amount)) {
+    ctx.reply('Please send a message in the format "number comment"');
+    return;
   }
+
+  // Save amount and comment in the session and proceed to category selection
+  ctx.session ??= {};
+  ctx.session.amount = amount;
+  ctx.session.comment = comment;
+
+  return ctx.reply(
+    'Select a category',
+    Markup.keyboard(categories).oneTime().resize()
+  );
+});
+
+async function submitToGoogleForm(amount, comment, category) {
+  // Implement function to submit data to Google Form
 }
 
 export const handler = async (event) => {
@@ -77,7 +54,7 @@ export const handler = async (event) => {
   }
 
   try {
-    return bot.handleUpdate(JSON.parse(event.body));
+    return await bot.handleUpdate(JSON.parse(event.body));
   } catch (e) {
     console.error(e);
     return { statusCode: 400, body: 'This was not a valid request.' };
